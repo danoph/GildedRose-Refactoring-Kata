2ee3c1b7a57752a62e520b91029ed22f8d13e697
Daniel Errante <magnet-dev+de@asynchrony.com>
2015-07-20 11:08:12 -0500

    making new class for sulfuras

--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -4,6 +4,25 @@ function Item(name, sell_in, quality) {
   this.quality = quality;
 }
 
+var ItemFactory = function(item) {
+  var self = this;
+
+  self.buildItem = function(item) {
+    if (item.name == 'Sulfuras, Hand of Ragnaros') {
+      return new SulfurasItem(item);
+    } else {
+      return new NormalItem(item);
+    }
+  };
+};
+
+var SulfurasItem = function(item) {
+  var self = this;
+
+  self.update = function() {
+  };
+};
+
 var NormalItem = function(item) {
   var self = this;
 
@@ -15,9 +34,7 @@ var NormalItem = function(item) {
 
   self.updateQuality = function() {
     if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-      if (item.name != 'Sulfuras, Hand of Ragnaros') {
-        subtract_quality(item);
-      }
+      subtract_quality(item);
 
       if (item.name == 'Conjured Mana Cake') {
         subtract_quality(item);
@@ -37,9 +54,7 @@ var NormalItem = function(item) {
   }
 
   self.updateSellIn = function() {
-    if (item.name != 'Sulfuras, Hand of Ragnaros') {
-      item.sell_in -= 1;
-    }
+    item.sell_in -= 1;
   };
 
   self.updateQuality2 = function() {
@@ -73,12 +88,11 @@ function add_quality(item) {
   }
 }
 
-function update_sell_in(item) {
-}
-
 function update_quality() {
+  var itemFactory = new ItemFactory();
+
   for (var i = 0; i < items.length; i++) {
-    var item = new NormalItem(items[i]);
+    var item = itemFactory.buildItem(items[i]);
     item.update();
   }
 }
